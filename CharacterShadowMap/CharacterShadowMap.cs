452078585bde32be59e4b65ca15e93adb132a174
Myeongseon Kim <myeongseon.kim@unity3d.com>
2023-03-25 00:57:56 +0900

    Implement CharacterShadowMap

---
+++ CharacterShadowMap/CharacterShadowMap.cs
@@ -0,0 +1,179 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+
+/// How to use
+/// 1. Create "CharacterShadow" Tag and set camera tag as "CharacterShadow"
+/// 2. Create camera and set rotation based on main directional light
+/// 3. Add Cinemachine component and Set Follow (Usually Hips Transform)
+/// 4. Set "Body - Transposer | Binding Mode = Lock To Target On Assign"
+///    & Set "Follow Offset" to adjust camera position
+/// 5. Set FOV
+public class CharacterShadowMap : ScriptableRendererFeature
+{
+    CharacterShadowPass m_Pass;
+    public static Camera lightCamera;
+    public RenderPassEvent injectionPoint = RenderPassEvent.BeforeRenderingPrePasses;
+    public ScriptableRenderPassInput requirements = ScriptableRenderPassInput.None;
+    public float bias;
+    public float stepOffset = 0.999f;
+
+    /// <inheritdoc/>
+    public override void Create()
+    {
+        m_Pass = new CharacterShadowPass(injectionPoint, RenderQueueRange.opaque, lightCamera);
+        m_Pass.ConfigureInput(requirements);
+        if (lightCamera == null)
+        {
+            lightCamera = GameObject.FindGameObjectWithTag("CharacterShadow")?.GetComponent<Camera>();
+        }
+    }
+
+    // Here you can inject one or multiple render passes in the renderer.
+    // This method is called when setting up the renderer once per-camera.
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        m_Pass.Setup("CharacterShadowMapRendererFeature", renderingData, bias, stepOffset);
+        renderer.EnqueuePass(m_Pass);
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        m_Pass.Dispose();
+    }
+
+
+    private class CharacterShadowPass : ScriptableRenderPass
+    {
+        private static readonly ShaderTagId k_ShaderTagId = new ShaderTagId("CharacterDepth");
+        private static int  s_CharShadowAtlasId = Shader.PropertyToID("_CharShadowAtlas");
+        private static int  s_CharShadowBias = Shader.PropertyToID("_CharShadowBias");
+        private static int  s_ViewMatrixId = Shader.PropertyToID("_CharShadowViewM");
+        private static int  s_ProjMatrixId = Shader.PropertyToID("_CharShadowProjM");
+        private static int  s_ShadowOffset0Id = Shader.PropertyToID("_CharShadowOffset0");
+        private static int  s_ShadowOffset1Id = Shader.PropertyToID("_CharShadowOffset1");
+        private static int  s_ShadowMapSize = Shader.PropertyToID("_CharShadowmapSize");
+        private static int  s_ShadowStepOffset = Shader.PropertyToID("_CharShadowStepOffset");
+        private static int  s_atlasSize = 4096;
+
+
+        private RTHandle m_Destination;
+        private ShaderTagId shaderTagId { get; set; } = k_ShaderTagId;
+        private ProfilingSampler m_ProfilingSampler;
+        private PassData m_PassData;
+
+        FilteringSettings m_FilteringSettings;
+
+        public CharacterShadowPass(RenderPassEvent evt, RenderQueueRange renderQueueRange, Camera lightCamera)
+        {
+            m_PassData = new PassData();
+            m_FilteringSettings = new FilteringSettings(renderQueueRange);
+            renderPassEvent = evt;
+            this.shaderTagId = k_ShaderTagId;
+            if (lightCamera != null)
+            {
+                m_PassData.viewM = lightCamera.worldToCameraMatrix;
+                m_PassData.projectM = lightCamera.projectionMatrix;
+            }
+        }
+
+        public void Dispose()
+        {
+            m_Destination?.Release();
+        }
+
+        public void Setup(string featureName, in RenderingData renderingData, float bias, float stepOffset)
+        {
+            m_ProfilingSampler = new ProfilingSampler(featureName);
+            m_PassData.bias = bias;
+            m_PassData.stepOffset = stepOffset;
+        }
+
+        public override void OnCameraSetup(CommandBuffer cmd, ref RenderingData renderingData)
+        {
+            // TODO: Create RT & bind
+            // var descriptor = new RenderTextureDescriptor(s_atlasSize, s_atlasSize, RenderTextureFormat.RGB111110Float, 0);
+            var descriptor = new RenderTextureDescriptor(s_atlasSize, s_atlasSize, RenderTextureFormat.Shadowmap, 32);
+            // RTHandles.Alloc(descriptor, FilterMode.Point, name:"_CharShadowAtlas");
+            RenderingUtils.ReAllocateIfNeeded(ref m_Destination, descriptor, FilterMode.Bilinear, name:"_CharShadowAtlas");
+            cmd.SetGlobalTexture(s_CharShadowAtlasId, m_Destination.nameID);
+
+            ConfigureTarget(m_Destination);
+            ConfigureClear(ClearFlag.All, Color.black);
+            m_PassData.targetHandle = m_Destination;
+        }
+
+        // Cleanup any allocated resources that were created during the execution of this render pass.
+        public override void OnCameraCleanup(CommandBuffer cmd)
+        {
+            
+        }
+
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            m_PassData.shaderTagId = this.shaderTagId;
+            m_PassData.filteringSettings = m_FilteringSettings;
+            m_PassData.profilingSampler = m_ProfilingSampler;
+
+            ExecutePass(context, m_PassData, ref renderingData);
+        }
+
+        private static void ExecutePass(ScriptableRenderContext context, PassData passData, ref RenderingData renderingData)
+        {
+            var cmd = CommandBufferPool.Get();
+            var filteringSettings = passData.filteringSettings;
+
+            float invShadowAtlasWidth = 1.0f / s_atlasSize;
+            float invShadowAtlasHeight = 1.0f / s_atlasSize;
+            float invHalfShadowAtlasWidth = 0.5f * invShadowAtlasWidth;
+            float invHalfShadowAtlasHeight = 0.5f * invShadowAtlasHeight;
+
+            using (new ProfilingScope(cmd, passData.profilingSampler))
+            {
+                context.ExecuteCommandBuffer(cmd);
+                cmd.Clear();
+
+                cmd.SetGlobalFloat(s_CharShadowBias, passData.bias);
+                cmd.SetGlobalMatrix(s_ViewMatrixId, passData.viewM);
+                cmd.SetGlobalMatrix(s_ProjMatrixId, passData.projectM);
+
+                // Soft shadow
+                cmd.SetGlobalVector(s_ShadowOffset0Id,
+                    new Vector4(-invHalfShadowAtlasWidth, -invHalfShadowAtlasHeight,
+                        invHalfShadowAtlasWidth, -invHalfShadowAtlasHeight));
+                cmd.SetGlobalVector(s_ShadowOffset1Id,
+                    new Vector4(-invHalfShadowAtlasWidth, invHalfShadowAtlasHeight,
+                        invHalfShadowAtlasWidth, invHalfShadowAtlasHeight));
+                cmd.SetGlobalVector(s_ShadowMapSize, new Vector4(invShadowAtlasWidth,
+                    invShadowAtlasHeight,
+                    s_atlasSize, s_atlasSize));
+                cmd.SetGlobalFloat(s_ShadowStepOffset, passData.stepOffset);
+
+                // var sortFlags = renderingData.cameraData.defaultOpaqueSortFlags;
+                var drawSettings = RenderingUtils.CreateDrawingSettings(passData.shaderTagId, ref renderingData, SortingCriteria.CommonOpaque);
+                drawSettings.perObjectData = PerObjectData.None;
+
+                context.DrawRenderers(renderingData.cullResults, ref drawSettings, ref filteringSettings);
+            }
+            // cmd.SetGlobalTexture(s_CharShadowAtlasId, passData.targetHandle.nameID);
+            context.ExecuteCommandBuffer(cmd);
+            CommandBufferPool.Release(cmd);
+        }
+
+        private class PassData
+        {
+            public RTHandle targetHandle;
+            public ShaderTagId shaderTagId;
+            public FilteringSettings filteringSettings;
+            public ProfilingSampler profilingSampler;
+            public Matrix4x4 viewM;
+            public Matrix4x4 projectM;
+            public float bias;
+            public float stepOffset;
+
+        }
+    }
+}
+
+
---
+++ CharacterShadowMap/DeclareCharacterShadowTexture.hlsl
@@ -0,0 +1,79 @@
+#ifndef CHARACTER_SHADOW_DEPTH_INPUT_INCLUDED
+#define CHARACTER_SHADOW_DEPTH_INPUT_INCLUDED
+
+#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Input.hlsl"
+#include "Packages/com.unity.render-pipelines.core/ShaderLibrary/Shadow/ShadowSamplingTent.hlsl"
+
+float _CharShadowBias;
+float4x4 _CharShadowViewM;
+float4x4 _CharShadowProjM;
+TEXTURE2D(_CharShadowAtlas);
+SAMPLER(sampler_CharShadowAtlas);
+float4 _CharShadowOffset0;
+float4 _CharShadowOffset1;
+float4 _CharShadowmapSize;
+float _CharShadowStepOffset;
+
+float3 ApplyShadowBias(float3 positionWS, float3 lightDirection)
+{
+    positionWS = lightDirection * _CharShadowBias + positionWS;
+    return positionWS;
+}
+
+float3 CharShadowWorldToView(float3 positionWS)
+{
+    return mul(_CharShadowViewM, float4(positionWS, 1.0)).xyz;
+}
+float4 CharShadowViewToHClip(float3 positionVS)
+{
+    return mul(_CharShadowProjM, float4(positionVS, 1.0));
+}
+float4 CharShadowWorldToHClip(float3 positionWS)
+{
+    return CharShadowViewToHClip(CharShadowWorldToView(positionWS));
+}
+float4 CharShadowObjectToHClip(float3 positionOS)
+{
+    float3 positionWS = mul(UNITY_MATRIX_M, float4(positionOS, 1.0));
+    positionWS = ApplyShadowBias(positionWS, _MainLightPosition.xyz);
+    return CharShadowWorldToHClip(positionWS);
+}
+
+half SampleCharacterShadowmap(float2 uv, float z)
+{
+    float var = SAMPLE_TEXTURE2D(_CharShadowAtlas, sampler_CharShadowAtlas, uv).r;
+    return var >= z;
+}
+
+half SampleCharacterShadowmapFiltered(float2 uv, float z)
+{
+    real fetchesWeights[9];
+    real2 fetchesUV[9];
+    SampleShadow_ComputeSamples_Tent_5x5(_CharShadowmapSize, uv, fetchesWeights, fetchesUV);
+
+    // real fetchesWeights[16];
+    // real2 fetchesUV[16];
+    // SampleShadow_ComputeSamples_Tent_7x7(_CharShadowmapSize, uv, fetchesWeights, fetchesUV);
+
+    float attenuation = fetchesWeights[0] * SampleCharacterShadowmap(fetchesUV[0].xy, z)
+                + fetchesWeights[1] * SampleCharacterShadowmap(fetchesUV[1].xy, z)
+                + fetchesWeights[2] * SampleCharacterShadowmap(fetchesUV[2].xy, z)
+                + fetchesWeights[3] * SampleCharacterShadowmap(fetchesUV[3].xy, z)
+                + fetchesWeights[4] * SampleCharacterShadowmap(fetchesUV[4].xy, z)
+                + fetchesWeights[5] * SampleCharacterShadowmap(fetchesUV[5].xy, z)
+                + fetchesWeights[6] * SampleCharacterShadowmap(fetchesUV[6].xy, z)
+                + fetchesWeights[7] * SampleCharacterShadowmap(fetchesUV[7].xy, z)
+                + fetchesWeights[8] * SampleCharacterShadowmap(fetchesUV[8].xy, z);
+                // + fetchesWeights[9] * SampleCharacterShadowmap(fetchesUV[9].xy, z);
+                // + fetchesWeights[10] * SampleCharacterShadowmap(fetchesUV[10].xy, z);
+                // + fetchesWeights[11] * SampleCharacterShadowmap(fetchesUV[11].xy, z);
+                // + fetchesWeights[12] * SampleCharacterShadowmap(fetchesUV[12].xy, z);
+                // + fetchesWeights[13] * SampleCharacterShadowmap(fetchesUV[13].xy, z);
+                // + fetchesWeights[14] * SampleCharacterShadowmap(fetchesUV[14].xy, z);
+                // + fetchesWeights[15] * SampleCharacterShadowmap(fetchesUV[15].xy, z);
+
+    return 1.0 - step(attenuation, _CharShadowStepOffset);
+    // return 0;
+}
+
+#endif
